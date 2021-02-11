f682efdb46e7ba4eab6b68489a5b8c4899989193
fall2020215 <71527395+fall2020215@users.noreply.github.com>
2021-02-10 23:08:31 -0500

    All things - except visualization

--- drive.py
+++ drive.py
@@ -28,14 +28,6 @@ with open ("maze.txt") as file:
             list_mid.append(n.Node(list_each_node[0],list_each_node[1],int(list_each_node[2]),int(list_each_node[3]),i-1,j-1))
         list_node.append(list_mid)
 
-#example to connect data member of the node number 8 at row 0 and column 7. The list_node is the same to an array 2D
-#print(list_node[1][0].get_name())
-#print(list_node[1][0].get_direction())
-#print(list_node[1][0].get_x())
-#print(list_node[1][0].get_y())
-#print(list_node[1][0].get_row())
-#print(list_node[1][0].get_column())
-
 
 #add all nodes from list_node to hashtable_node --> to look at quickly where a node is the list_node
 # key of hashtable_node is the name of node, and the value is the tuple of (row, column) in the array (list_node)
@@ -43,9 +35,6 @@ for row in range (1, int(size_maze.split()[0]) + 1):
     for column in range (1, int(size_maze.split()[1]) + 1):
         name_node = list_node[row-1][column-1].get_name()
         hashtable_node[name_node] = (row-1, column-1)
-
-#print(hashtable_node)
-#print(len(hashtable_node))
 
 
 #get position final_x and final_y of final node
@@ -56,68 +45,7 @@ final_y = list_node[final_row][final_column].get_y()
 ################### FINISH PART I #############################################################
 
 
-################### BEGINNING ---  A* Algorithm for shortest path #############################
-
-
-
-#################################
-#WE HAVE TO CHANGE THE FIRST NODE
-##################################
-'''
-first_name = 'R26'
-first_neighbor = a.neighbor_nodes(first_name,hashtable_node, list_node)
-
-row_first_node, column_first_node = hashtable_node[first_name]
-first_node = list_node[row_first_node][column_first_node]
-'''
-
-
-
-#################################
-#CHANGE THE FIRST NODE
-#################################
-
-#innital for current_heap and hashtable_heap for first node:
-#heapq.heappush(customers, (9, "Stacy"))
-'''
-distance_straight = a.distance_straight_to_final_node(first_name,final_x,final_y,hashtable_node, list_node)
-heapq.heappush(current_heap, (distance_straight, (first_name, 0, distance_straight, first_name)))
-hashtable_heap[first_name] = (first_name, 0 , distance_straight, first_name)
-
-'''
-
-
-#print(heapq.heappop(current_heap))
-#print(hashtable_heap)
-#print(heapq.heappop(current_heap)[1][0])
-#print(heapq.heappop(current_heap)[0])
-
-################### ENDING  --- A* Algorithm for shortest path ################################
-
-#print(a.distance_straight_to_final_node('B16',final_x, final_y, hashtable_node, list_node))
-#print(hashtable_node['B16'])
-#print(a.distance_between_two_nodes('R0','B16',hashtable_node, list_node))
-
-'''
-THe result will include the final node "O" in the list.
-neighbor = a.neighbor_nodes('B16',hashtable_node, list_node)
-print(len(neighbor))
-for element in neighbor:
-
-    print(element.get_name())
-'''
-
-#a.shortest_path(current_heap,hashtable_node,hashtable_heap,list_node,final_x,final_y,'O')
-
-#FOR FEWEST NODE PATH
-'''
-heapq.heappush(current_heap, (1, (first_name, 0, 1, first_name)))
-hashtable_heap[first_name] = (first_name, 0 , 1, first_name)
-'''
-
-
-#a.fewest_node_path(current_heap,hashtable_node,hashtable_heap,list_node,final_x,final_y,'O')
-
+################### MAIN FUNCTION #############################################################
 
 print()
 print("Select one of A* algorithm heuristics following:\n")
@@ -185,4 +113,4 @@ else:
 
 
 
-
+################# FINISH MAIN FUNCTION #######################################################################
